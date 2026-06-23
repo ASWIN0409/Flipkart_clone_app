177a3d5b1dcafb66f99e78a9feb2cf290ef3f6b3
ASWIN0409 <aswinsri.sg@gmail.com>
2026-06-24 00:21:38 +0530

    Integrated Tanstack query

---
+++ frontend/src/api/fetchProductList.js
@@ -0,0 +1,6 @@
+import { axiosInstance } from "../helpers/axiosInstance";
+
+export default async function fetchProductList() {
+        const response = await axiosInstance.get('api/product/');
+        return response.data;
+}
--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,7 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import fetchProductList from "../api/fetchProductList";
+
 function ProductList() {
 
+    const BASE_URL = import.meta.env.VITE_BASE_URL
+
+    const { data: products, isLoading, isError, error } = useQuery({
+        queryKey: ['products'],
+        queryFn: fetchProductList,
+        staleTime: 1000 * 60 * 2,
+        gcTime: 1000 * 60 * 2,
+        retry: 2,
+        refetchOnWindowFocus: true
+    });
+
+    console.log(products);
+
+    if (isLoading) return <h1>Loading...</h1>
+    if (isError) return <h1>Error: {error.message}</h1>
+
     return (
-        <div>This is the product list page</div>
+        <>
+            <div>This is the product list page</div>
+            <div>
+                {products && products.map(product => (
+                    <div key={product.id}>
+                        <img className="h-20 w-20" src={`${BASE_URL}${product.image}`} alt={product.name} />
+                        <p>{product.name}</p>
+                        <p>{product.price}</p>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
 
