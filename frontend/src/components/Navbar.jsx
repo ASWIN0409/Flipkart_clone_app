110b6ecf45f9b370296c7a8b2724e8ffe8a99dd6
ASWIN0409 <aswinsri.sg@gmail.com>
2026-06-24 01:10:18 +0530

    Added navbar component and product details page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,65 @@
+import { useNavigate, Link } from 'react-router-dom';
+
 function Navbar() {
+
+    const navigate = useNavigate();
+
     return (
-        <header>
-            this is header
+        <header className="bg-white shadow-md sticky top-0 z-50">
+            <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+
+                {/* Logo */}
+                <h1 onClick={() => navigate('/')} className="text-2xl font-bold text-indigo-600 cursor-pointer">
+                    Flipkart Clone
+                </h1>
+
+                {/* Navigation */}
+                <nav className="hidden md:flex gap-8 font-medium text-gray-700">
+                    <Link className="hover:text-indigo-600 transition">
+                        Home
+                    </Link>
+                    <Link to={'/'} className="hover:text-indigo-600 transition">
+                        Products
+                    </Link>
+                    <Link className="hover:text-indigo-600 transition">
+                        Categories
+                    </Link>
+                    <Link className="hover:text-indigo-600 transition">
+                        Contact
+                    </Link>
+                </nav>
+
+                {/* Search Bar */}
+                <div className="hidden lg:flex">
+                    <input
+                        type="text"
+                        placeholder="Search products..."
+                        className="border border-gray-300 rounded-l-lg px-4 py-2 w-64 outline-none focus:ring-2 focus:ring-indigo-500"
+                    />
+                    <button className="bg-indigo-600 text-white px-4 rounded-r-lg hover:bg-indigo-700 transition">
+                        Search
+                    </button>
+                </div>
+
+                {/* Right Side */}
+                <div className="flex items-center gap-4">
+
+                    <button className="relative bg-gray-100 p-2 rounded-lg hover:bg-gray-200 transition">
+                        🛒
+                        <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs px-2 rounded-full">
+                            3
+                        </span>
+                    </button>
+
+                    <button className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition">
+                        Login
+                    </button>
+                    <button className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition">
+                        Register
+                    </button>
+
+                </div>
+            </div>
         </header>
     );
 }
--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -1,11 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import fetchProductDetails from "../api/fetchProductDetails";
 
 function ProductDetails() {
 
-    const {id} = useParams();
+    const { id } = useParams();
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+    const { data: product, isLoading, isError, error } = useQuery({
+        queryKey: ['product', id],
+        queryFn: () => fetchProductDetails(id),
+        staleTime: 1000 * 60 * 2,
+        gcTime: 1000 * 60 * 2,
+        retry: 2,
+        refetchOnWindowFocus: true
+    });
+
+    if (isLoading) return <h1>Loading...</h1>
+    if (isError) return <h1>Error: {error.message}</h1>
 
     return (
-        <div>This is the product details page for id: {id}</div>
+        <>
+            <div>This is the product detail page {id}</div>
+            <div className=" w-80 h-full mx-auto bg-white rounded-3xl overflow-hidden shadow-md">
+
+                <div className="relative overflow-hidden">
+                    <img
+                        src={`${BASE_URL}${product.image}`}
+                        alt={product.name}
+                        className="h-56 w-full"
+                    />
+                </div>
+
+                <div className="p-5">
+                    <h2 className="font-semibold text-gray-800 text-lg">
+                        {product.name}
+                    </h2>
+
+                    <div className="flex items-center justify-between mt-3">
+                        <span className="text-2xl font-bold text-green-600">
+                            ₹{product.price}
+                        </span>
+
+                        <button className="bg-black text-white px-4 py-2 rounded-lg hover:bg-gray-800">
+                            Add to Cart
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </>
     );
 }
 
