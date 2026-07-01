f3ef233ead98bdfe011e1de0e87e035299060690
ASWIN0409 <aswinsri.sg@gmail.com>
2026-07-01 13:53:00 +0530

    Added React error boundary

--- frontend/src/pages/Routing.jsx
+++ frontend/src/pages/Routing.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import ProductDetails from "./ProductDetails";
 import ProductList from "./ProductList";
@@ -7,16 +7,35 @@ import Checkout from "./Checkout";
 import Login from "./Login";
 import Register from "./Register";
 import PrivateRoute from "./Auth/PrivateRoute";
+import { ErrorBoundary } from "react-error-boundary";
+import CustomErrorBoundary from "../components/ErrorBoundary/CustomErrorBoundary";
 
 function Routing() {
+
+    const location =  useLocation();
+
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
 
             <Route path="/" element={<MainLayout />}>
-                <Route index element={<ProductList />} />
-                <Route path="product/details/:id" element={<ProductDetails />} />
+                <Route index element={
+                    <ErrorBoundary
+                        FallbackComponent={CustomErrorBoundary}
+                        onReset={[location.pathname]}
+                    >
+                        <ProductList />
+                    </ErrorBoundary>
+                } />
+                <Route path="product/details/:id" element={
+                    <ErrorBoundary
+                        FallbackComponent={CustomErrorBoundary}
+                        onReset={[location.pathname]}
+                    >
+                        <ProductDetails />
+                    </ErrorBoundary>
+                } />
 
                 {/* Protected routes */}
                 <Route element={<PrivateRoute />}>
